82010cef33bbf8d604793a2295adb00be477b6fa
noa <142442536+barrionuevo77@users.noreply.github.com>
2024-05-20 23:11:01 -0300

    html,css y java
    fijarse pq la nav no funciona en el html de mytery box y como se ve en pc, modificar para que no se vea tan deforme :)

--- js/nav.js
+++ js/nav.js
@@ -2,6 +2,7 @@
 let navUl = document.getElementById("nav-ul");
 let navBtnOpen = document.getElementById("nav-btn-open");
 let navBtnClose = document.getElementById("nav-btn-close");
+
 let open = navBtnOpen.addEventListener("click", function(){
 
     navUl.classList.remove("hidden");
@@ -13,3 +14,10 @@ let close = document.getElementById("nav-btn-close").addEventListener("click", f
     navBtnOpen.classList.remove("hidden");
     navBtnClose.classList.add("hidden");
 });
+
+/* para que se vea EN PC NORMAL */
+document.querySelector(".btn-menu").addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    document.querySelector(".nav-ul").classList.toggle("show");
+}
