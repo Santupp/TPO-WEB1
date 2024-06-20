fece2d7a73574559563cdee6caff1aec769259f0
santino <santino.monniello@gmail.com>
2024-06-20 18:56:22 -0300

    fixed table element width, added ids to each table data

--- js/rest.js
+++ js/rest.js
@@ -1,6 +1,6 @@
 let form = document.getElementById("formulario");
 let tablaInfo = document.getElementById("tabla-info");
-    
+
 form.addEventListener("submit", api);
 function api(event){
     event.preventDefault();
@@ -34,16 +34,22 @@ function api(event){
 cargarTabla();
 function cargarTabla(){
     const url =  'https://6671f1bee083e62ee43da268.mockapi.io/user';
-    
+    let btn = document.querySelectorAll(".tabla-option-btn");
+
     fetch(url) 
     .then(res => res.json())
     .then(json => {
         for(let i = 0; i < json.length; i++){
             tablaInfo.innerHTML += 
-                "<tr>" +
+                "<tr id=" +json[i].id+ ">" +
                 "<td class='tabla-nombre'>" + json[i].name + "</td>" +
                 "<td class='tabla-email'>" + json[i].email + "</td>" + 
+                "<td class='tabla-option-btn'><button class='tabla-eliminar-btn' type='button'>Eliminar</button></td>"+
                 "</tr>";
+            
+                btn.addEventListener("click", function(){
+
+                })
         }
     })
     .catch(e => console.log(e));
